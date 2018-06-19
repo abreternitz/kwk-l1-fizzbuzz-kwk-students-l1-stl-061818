888e56897af045ffa88936b83ff691026ef70966
abreternitz <aliceburian@gmail.com>
2018-06-19 18:40:53 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,17 +2,20 @@
 # if a number is divisible by 5, return buzz
 #if a number is divisible by both 3 AND 5, return fizzbuzz
 
-def fizzbuzz(x=gets.chomp)
+def fizzbuzz(x)
   if  x%5==0 && x%3==0
-    puts "fizz"
+    puts "fizzbuzz"
     
   elsif x%5==0 && x%3!=0
     puts "buzz"
   
   elsif x%3==0 && x%5!=0
-    puts "fizzbuzz"
-
+    puts "fizz"
+  
+  elsif x%3!=0 && x%5!=0
+    puts "nope!"
+  
   end
 end  
 
-fizzbuzz()
+fizzbuzz(7)
