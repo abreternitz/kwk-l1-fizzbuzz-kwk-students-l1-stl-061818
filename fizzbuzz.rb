1a1c6672be7e0e28f05c548dd52e19aa31a8eefb
abreternitz <aliceburian@gmail.com>
2018-06-19 18:22:12 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,12 +3,12 @@
 #if a number is divisible by both 3 AND 5, return fizzbuzz
 
 def fizzbuzz(x)
-  if x%3==0 && x%5!=0
+  if x%3==0&&x%5!=0
     puts "fizz"
-  else x%5==0 && x%3!=0
+  else x%5==0&&x%3!=0
     puts "buzz"
   end 
-  else if x%5==0 && x%3==0
+  else if x%5==0&&x%3==0
     puts "fizzbuzz"
   end
 end  
