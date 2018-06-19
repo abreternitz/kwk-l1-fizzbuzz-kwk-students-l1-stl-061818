85d36db9170319443bcb309fb3a44367a79d9715
abreternitz <aliceburian@gmail.com>
2018-06-19 18:12:17 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,19 @@
-def fizz(number)
-  number=gets.chomp
-  if
-    number==3
-  end
-end  
+# if a number is divisible by 3, return fizz
+# if a number is divisible by 5, return buzz
+#if a number is divisible by both 3 AND 5, return fizzbuzz
+
+def fizzbuzz(x)
+  x=gets.chomp
+  puts "you gave me #{x}"
+  
+#   if(x)
+
+# else
+#   else  
+  
+# end
+#   else if
+  
+#   end
+  
+# end  
